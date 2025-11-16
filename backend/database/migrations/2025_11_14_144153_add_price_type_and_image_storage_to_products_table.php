1d58328c50428f9892cc55986106580cc41411a5
MAYEEN <uddin.md.mayeen@ict.inc>
2025-11-16 23:13:44 +0900

    Migration changed

--- backend/database/migrations/2025_11_14_144153_add_price_type_and_image_storage_to_products_table.php
+++ backend/database/migrations/2025_11_14_144153_add_price_type_and_image_storage_to_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             $table->string('currency')->default('AUD')->after('price');
-            $table->string('image_storage')->default('local')->after('image_url');
+            $table->string('image_storage')->default('local')->after('image');
         });
     }
 
